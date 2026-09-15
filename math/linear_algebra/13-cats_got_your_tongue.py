c8bd6e0edbdbfa54ac25d813c8e5357974b4474e
Noella799 <n.uwera@alustudent.com>
2026-09-15 07:06:49 -0700

    Add linear algebra tasks 11 to 14

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Concatenate two NumPy arrays."""
+
+
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    """Return a new array concatenating mat1 and mat2."""
+    return np.concatenate((mat1, mat2), axis=axis)
---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Perform matrix multiplication with NumPy."""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Return the matrix product of mat1 and mat2."""
+    return np.matmul(mat1, mat2)
